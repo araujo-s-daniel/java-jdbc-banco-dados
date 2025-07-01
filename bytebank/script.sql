77ec1e86b6348feac07109767158db5a9f3ff9aa
Daniel Sena <daniels_araujo@outlook.com.br>
2025-07-01 10:19:06 -0300

    01.04 - Para Saber mais: Instalação do MySQL e configuração do banco de dados

---
+++ bytebank/script.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE byte_bank;
+
+USE byte_bank;
+
+CREATE TABLE conta (
+    numero INT NOT NULL,
+    saldo DECIMAL(10, 0),
+    cliente_nome VARCHAR(50),
+    cliente_cpf VARCHAR(11),
+    cliente_email VARCHAR(50),
+    PRIMARY KEY (numero)
+);

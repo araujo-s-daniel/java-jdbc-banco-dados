8d1e625a4d2d8074a7339ce8fdb90c8c1d1ff212
Daniel Sena <daniels_araujo@outlook.com.br>
2025-07-01 18:40:33 -0300

    03.02 - Preparando para listar contas

--- bytebank/src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
+++ bytebank/src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
@@ -1,14 +1,18 @@
 package br.com.alura.bytebank.domain.conta;
 
 import br.com.alura.bytebank.domain.cliente.Cliente;
+import br.com.alura.bytebank.domain.cliente.DadosCadastroCliente;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContaDAO {
-    private Connection conn;
+    private final Connection conn;
 
     ContaDAO(Connection connection) {
         this.conn = connection;
@@ -20,7 +24,8 @@ public class ContaDAO {
 
         String sql = "INSERT INTO conta (numero, saldo, cliente_nome, cliente_cpf, cliente_email)" +
                 " VALUES (?, ?, ?, ?, ?)";
-        PreparedStatement preparedStatement = null;
+        PreparedStatement preparedStatement;
+
         try {
             preparedStatement = conn.prepareStatement(sql);
             preparedStatement.setInt(1, conta.getNumero());
@@ -32,5 +37,31 @@ public class ContaDAO {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public Set<Conta> listar() {
+        Set<Conta> contas = new HashSet<>();
+        String sql = "SELECT * FROM conta";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                Integer numero = resultSet.getInt(1);
+                BigDecimal saldo = resultSet.getBigDecimal(2);
+                String nome = resultSet.getString(3);
+                String cpf = resultSet.getString(4);
+                String email = resultSet.getString(5);
+
+                DadosCadastroCliente dadosCadastroCliente = new DadosCadastroCliente(nome, cpf, email);
+                Cliente cliente = new Cliente(dadosCadastroCliente);
+                contas.add(new Conta(numero, cliente));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return contas;
     }
 }
